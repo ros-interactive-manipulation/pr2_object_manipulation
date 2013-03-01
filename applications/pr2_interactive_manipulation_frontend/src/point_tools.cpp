446d5abaa23382c6558480ac1f4fb01efca0e46c
jon-weisz <jon.weisz@gmail.com>
2013-02-28 22:39:45 -0800

    Set up gripper click button to publish poses instead of points using the newly implemented rviz get3DPatch

--- applications/pr2_interactive_manipulation_frontend/src/point_tools.cpp
+++ applications/pr2_interactive_manipulation_frontend/src/point_tools.cpp
@@ -31,6 +31,24 @@
 
 #include <rviz/properties/string_property.h>
 
+#include <pcl_ros/point_cloud.h>
+#include <pcl/features/integral_image_normal.h>
+
+#include <geometry_msgs/Point.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <OGRE/OgreVector3.h>
+#include <OGRE/OgreViewport.h>
+#include <OGRE/OgreCamera.h>
+#include <rviz/display_context.h>
+#include <rviz/selection/selection_manager.h>
+#include <rviz/properties/int_property.h>
+#include <tf/transform_datatypes.h>
+#include <visualization_msgs/Marker.h>
+
+
+
+
+
 namespace pr2_interactive_manipulation
 {
 
@@ -41,8 +59,161 @@ LookAtTool::LookAtTool()
 
 SetGripperTool::SetGripperTool()
 {
-  topic_property_->setStdString("/rviz/set_gripper");
+  createProperties();
+  topic_property_->setStdString("/rviz/set_gripper_point"); 
+  topic_property_pose_->setStdString("/cloud_click_point");
+  pub_pose_ = nh_.advertise<geometry_msgs::PoseStamped>( topic_property_pose_->getStdString(), 1 );
 };
+
+void SetGripperTool::createProperties()
+{
+  topic_property_pose_ = new rviz::StringProperty("Pose Topic",
+                                                  "/cloud_click_point", "Topic on which to publish poses.",
+                                                  getPropertyContainer(),
+                                                  SLOT( updatePoseTopic() ), this);
+
+  // number of pixels around point to sample for depth image
+  normal_box_padding_ = new rviz::IntProperty("Normal box padding",
+                                              7,
+                                              "Size of box around clicked point to sample for normal");
+  
+  // Maximum far distance for depth image 
+  far_clip_distance_ = new rviz::FloatProperty("Far Clip Distance",
+                                              50.0,
+                                              "Maximum distance of depth selection");
+
+  
+}
+
+void SetGripperTool::updatePoseTopic()
+{
+  pub_pose_ = nh_.advertise<geometry_msgs::PoseStamped>( topic_property_pose_->getStdString(), 1 );
+}
+
+
+
+
+int SetGripperTool::processMouseEvent( rviz::ViewportMouseEvent& event )
+{
+  int flags = 0;  
+  Ogre::Vector3 pos;
+
+  // Test if the mouse is over anything
+  bool success = context_->getSelectionManager()->get3DPoint( event.viewport, event.x, event.y, pos );
+  setCursor( success ? hit_cursor_ : std_cursor_ );
+
+  // If we are actually over a point, set the tooltip appropriately
+  if ( success )
+  {
+    std::ostringstream s;
+    s << "<b>Left-Click:</b> Select this point.";
+    s.precision(3);
+    s << " [" << pos.x << "," << pos.y << "," << pos.z << "]";
+    setStatus( s.str().c_str() );
+    
+    // On click, get a patch of points in the region, and calculate the normal to the point.
+    if( event.leftUp() )
+    {
+      tf::Vector3 normal_vec(0,1,0); // Default normal
+
+      // Set maximum depth for projection
+      float starting_max_depth = event.viewport->getCamera()->getFarClipDistance();
+      event.viewport->getCamera()->setFarClipDistance(far_clip_distance_->getFloat());
+
+      
+      int bp = normal_box_padding_->getInt();
+      unsigned int box_width(2*bp+1);
+      
+      // Set up ordered point cloud
+      pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>(box_width, box_width));
+      cloud->reserve(box_width * box_width);
+      
+      // Get patch of points in region around click point
+      std::vector<Ogre::Vector3> box_pos;
+      bool success_box = context_->getSelectionManager()->get3DPatch( event.viewport, event.x, event.y, bp, true, box_pos );
+      
+      // Pack ogre points in to point cloud
+      for (unsigned int i = 0; i < box_pos.size(); ++ i)
+      {
+        (*cloud)[i] = pcl::PointXYZ(box_pos[i].x, box_pos[i].y, box_pos[i].z);         
+      }
+        
+
+      // If we retrieved a significant number of points, calculate the actual normal
+      if(box_pos.size() > 3)
+      {
+       pcl::IntegralImageNormalEstimation<pcl::PointXYZ, pcl::Normal> ne;
+       
+       Ogre::Vector3 camera_pos = event.viewport->getCamera()->getDerivedPosition();
+
+       ne.setViewPoint(camera_pos.x, camera_pos.y, camera_pos.z);
+       ne.setNormalEstimationMethod (ne.AVERAGE_3D_GRADIENT);
+       ne.setMaxDepthChangeFactor(0.02f);
+       ne.setNormalSmoothingSize(10.0f);
+       ne.setRectSize(bp - 1, bp -1);
+       ne.setInputCloud(cloud);
+
+       // Get index of center point
+       unsigned center_point_index = bp + ( box_width ) * bp;
+       pcl::Normal normal_point;
+       ne.computePointNormal(bp, bp, center_point_index, normal_point);
+
+       // If there was a normal point, generate normal and make sure it points in the right direction.
+       if (!std::isnan(normal_point.normal_x)){
+         
+         tf::Vector3 point_to_head(tf::Vector3(camera_pos.x - pos.x, camera_pos.y - pos.y, camera_pos.z - pos.z).normalized());
+         
+         normal_vec = tf::Vector3(normal_point.normal_x, normal_point.normal_y, normal_point.normal_z);           
+         
+         // Flip normal if necessary.
+         if(point_to_head.dot(normal_vec) < 0)
+           normal_vec *= -1;
+       }
+       else
+       {
+         ROS_WARN("Failed to find normal - using default %f %f %f", normal_vec.x(), normal_vec.y(), normal_vec.z());         
+       } 
+      }  
+                   
+      // Generate pose from point and normal -- Maybe do something smarter to align 
+      // gripper to curvature.
+      geometry_msgs::PoseStamped ps;
+    
+      ps.pose.position.x = pos.x;
+      ps.pose.position.y = pos.y;
+      ps.pose.position.z = pos.z;
+      ps.header.frame_id = context_->getFixedFrame().toStdString();
+      ps.header.stamp = ros::Time::now();
+      
+      tf::Vector3 Z = normal_vec.normalized();
+      tf::Vector3 Y = normal_vec.cross(tf::Vector3(0,0,1)).normalized();
+      tf::Vector3 X = Y.cross(normal_vec).normalized();
+      tf::Matrix3x3 mat(X.x(), Y.x(), Z.x(),
+                        X.y(), Y.y(), Z.y(),
+                        X.z(), Y.z(), Z.z());
+      tf::Quaternion q;
+      mat.getRotation(q);
+      tf::quaternionTFToMsg(q, ps.pose.orientation);
+          
+      pub_pose_.publish( ps );     
+      
+      if ( auto_deactivate_property_->getBool() )
+      {
+        flags |= Finished;
+      }
+      event.viewport->getCamera()->setFarClipDistance(starting_max_depth); 
+    }
+     
+  }
+  else
+  {
+    setStatus( "Move over an object to select the target point." );
+  }
+  // Reset clipping distance to default distance.
+
+  return flags;
+  
+}
 
 NavigateToTool::NavigateToTool()
 {
